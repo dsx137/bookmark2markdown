53d43888aaf0a07520e15d12a568d36f1fe98bda
dsx137 <70027572+dsx137@users.noreply.github.com>
2023-08-18 20:20:17 +0800

    fix

--- bookmark2markdown.py
+++ bookmark2markdown.py
@@ -11,38 +11,47 @@ def mdEscape(str, specialChars):
     return str
 
 
-def FindAndWriteInFile(sourceFile, outputFile, spaceBetween, haveBlankLine):
+def FindAndWriteInFile(sourceFile, outputFile, spaceBetween):
     lines = sourceFile.readlines()
+    tab_count = 0
     for line in lines:
-        hrefList = re.findall('<a href=".*?"', line, re.IGNORECASE)
-        iconList = re.findall('icon=".*?"', line, re.IGNORECASE)
-        titleList = re.findall(">[^>]+?</a>$", line, re.IGNORECASE)
         mdFileLine = ""
-        if titleList:
-            title = mdEscape(titleList[0][1 : len(titleList[0]) - 4], "[]|")
-            if iconList:
-                icon = iconList[0][6 : len(iconList[0]) - 1]
-                mdFileLine += "![image](" + icon + ")" + ("&nbsp;" * spaceBetween)
-            if hrefList:
-                href = hrefList[0][9 : len(hrefList[0]) - 1]
-                mdFileLine += "[" + title + "]" + "(" + href + ")"
-            if mdFileLine != "":
-                mdFileLine += "\n\n" if haveBlankLine else "\\\n"
+        # read until <DL><P>
+        if re.findall("<DL><P>", line, re.IGNORECASE):
+            tab_count += 1
+            continue
+        elif re.findall("</DL><P>", line, re.IGNORECASE):
+            tab_count -= 1
+            continue
+        else:
+            titleList = re.findall(">[^>]+?</H", line, re.IGNORECASE)
+            if titleList:
+                mdFileLine += ("  " * tab_count) + "+ "
+                title = mdEscape(titleList[0][1 : len(titleList[0]) - 3], "[]|")
+                mdFileLine += title
+            else:
+                hrefList = re.findall('<a href=".*?"', line, re.IGNORECASE)
+                iconList = re.findall('icon=".*?"', line, re.IGNORECASE)
+                titleList = re.findall(">[^>]+?</a>$", line, re.IGNORECASE)
+
+                if titleList:
+                    mdFileLine += ("  " * tab_count) + "+ "
+                    title = mdEscape(titleList[0][1 : len(titleList[0]) - 4], "[]|")
+                    if iconList:
+                        icon = iconList[0][6 : len(iconList[0]) - 1]
+                        mdFileLine += "![image](" + icon + ")" + ("&nbsp;" * spaceBetween)
+                    if hrefList:
+                        href = hrefList[0][9 : len(hrefList[0]) - 1]
+                        mdFileLine += "[" + title + "]" + "(" + href + ")"
+
+        if mdFileLine != "":
+            mdFileLine += "\n"
             outputFile.write(mdFileLine)
 
 
-bookmark_path = input("Input the bookmark file path:")
-output_dir = input("Input the output file dir (the file named 'output.md'):")
-spaceBetween = int(input("Input space number between icon and href:"))
-
-haveBlankLine = input("Is there BlankLine between Lines? Y/N")
-if haveBlankLine == "Y" or "y":
-    haveBlankLine = True
-elif haveBlankLine == "N" or "n":
-    haveBlankLine = False
-else:
-    print("Error! And It will be false.")
-    haveBlankLine = False
+bookmark_path = input("Input the bookmark file path:\n")
+output_dir = input("Input the output file dir (the file named 'output.md'):\n")
+spaceBetween = int(input("Input space number between icon and href:\n"))
 
 try:
     sourceFile = open(bookmark_path, mode="r", encoding="UTF-8")
@@ -51,6 +60,6 @@ except FileNotFoundError:
     exit(0)
 
 outputFile = open(output_dir + "/output.md", mode="w", encoding="UTF-8")
-FindAndWriteInFile(sourceFile, outputFile, spaceBetween, haveBlankLine)
+FindAndWriteInFile(sourceFile, outputFile, spaceBetween)
 outputFile.close()
 sourceFile.close()
